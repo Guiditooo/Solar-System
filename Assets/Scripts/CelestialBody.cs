4066f114dcc1fb2eb15fe0c4dc3cea8ada073c26
Guido Tello <lpmaleinn@gmail.com>
2022-03-17 11:48:44 -0300

    Added Rotation

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [SerializeField] private float diameter;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float traslationSpeed;
+
+    public GameObject referencePoint;
+    [SerializeField] private float distanceFromReferencePoint;
+
+
+    private void Awake()
+    {
+        transform.localScale = new Vector3(diameter, diameter, diameter);
+        
+    }
+    private void Start()
+    {
+        if (gameObject.layer != 6)
+        {
+            transform.position = new Vector3(referencePoint.transform.lossyScale.x * (1 + distanceFromReferencePoint), 0, 0);
+        }
+    }
+
+    private void Rotation()
+    {
+        Quaternion aux = transform.rotation;
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void Traslation()
+    {
+        transform.RotateAround(referencePoint.transform.position, Vector3.up, traslationSpeed * Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        Rotation();
+        Traslation();
+    }
+}
