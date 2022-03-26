8250ce6a68576ecce09a4a155af89f4b71faa8b7
Guido Tello <lpmaleinn@gmail.com>
2022-03-26 20:59:59 -0300

    Added MainMenu Scene

---
+++ Assets/Scripts/General.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class General : MonoBehaviour
+{
+    public enum SceneName
+    {
+        MainMenu,
+        Game,
+        End
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject credits;
+
+    private void Awake()
     {
-        
+        CloseCredits();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenCredits()
     {
-        
+        credits.gameObject.SetActive(true);
+        menu.gameObject.SetActive(false);
     }
+    public void CloseCredits()
+    {
+        credits.gameObject.SetActive(false);
+        menu.gameObject.SetActive(true);
+    }
+
+    public static void LoadGameScene()
+    {
+        SceneManager.LoadScene((int)General.SceneName.Game);
+    }
+
+    public static void CloseGame()
+    {
+        Application.Quit();
+    }
+
 }
